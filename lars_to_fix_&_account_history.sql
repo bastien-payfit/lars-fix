13d36b9692598a6068ff4d5a8de1fa43f79db534
bastienvelitchkine <bastienvelitchkine@bastien.velitchkine>
2021-04-23 18:50:05 +0100

    Check if lar owners are the first owners recorded in account history when no owner *change* recorded in accounthistory

--- lars_to_fix_&_account_history.sql
+++ lars_to_fix_&_account_history.sql
@@ -67,3 +67,61 @@ FROM lars_to_fix
 LEFT JOIN history 
     on lars_to_fix.account__c = history.accountid
 WHERE history.accountid is null;
+
+-- @block Is lar owner account first owner when no account history?
+WITH lars_w_owner AS (
+    SELECT
+        id,
+        account__c,
+        assignement_date__c,
+        end_relation_date__c,
+        owner_relation__c
+    FROM data.staging_salesforce.batchaccountrelation__c
+    WHERE
+        NOT isdeleted
+        AND owner_relation__c IS NOT NULL
+),
+history1 AS (
+    SELECT 
+        accountid, 
+        createddate, 
+        oldvalue__string, 
+        newvalue__string 
+    FROM staging_salesforce.accounthistory
+    WHERE 
+        field = 'Owner' 
+        and oldvalue__string <> newvalue__string
+        and oldvalue__string not like '0053X%'
+        and newvalue__string <> 'Outbound Database' 
+        and newvalue__string not like '%Reassignment%'
+),
+lars_w_owner_no_history AS (
+    SELECT 
+        lars_w_owner.id,
+        lars_w_owner.account__c,
+        lars_w_owner.owner_relation__c
+    FROM lars_w_owner
+    LEFT JOIN history1 
+        on lars_w_owner.account__c = history1.accountid
+    WHERE history1.accountid is null
+),
+history2 AS (
+    SELECT 
+        accountid, 
+        createddate, 
+        oldvalue__string, 
+        newvalue__string 
+    FROM staging_salesforce.accounthistory
+    WHERE 
+        field = 'Owner' 
+        and oldvalue__string not like '0053X%'
+        --and newvalue__string <> 'Outbound Database' 
+        --and newvalue__string not like '%Reassignment%'
+)
+SELECT
+    lars_w_owner_no_history.*,
+    history2.* 
+FROM lars_w_owner_no_history
+JOIN history2 
+ON lars_w_owner_no_history.account__c = history2.accountid
+;
